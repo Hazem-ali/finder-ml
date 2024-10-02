5741b7965c664ec94f105f5f098cf55a6ddd1926
hazem <hazemali627@gmail.com>
2024-10-02 22:39:25 +0300

    caching ml results

--- detector.py
+++ detector.py
@@ -1,4 +1,5 @@
 import face_recognition
+import numpy as np
 from io import BytesIO
 
 
@@ -19,3 +20,55 @@ def is_same_person(first_photo, second_photo, threshold=0.6):
     else:
         print("Could not find faces in one or both images.")
         return False
+
+
+
+def is_same_person_optimized(
+    first_image_encoding: list, second_image_encoding: list, threshold: float = 0.6
+) -> bool:
+    """
+    Compares two face encodings to determine if they belong to the same person.
+
+    Args:
+    first_image_encoding (list): The encoding of the first image (from the database or new image).
+    second_image_encoding (list): The encoding of the second image (from the database or new image).
+    threshold (float): The threshold to determine if the two encodings are close enough to be considered the same person.
+
+    Returns:
+    bool: True if the two encodings are similar enough to represent the same person, False otherwise.
+    """
+
+    # print(first_image_encoding)
+    # print(second_image_encoding)
+
+    if not first_image_encoding or not second_image_encoding:
+        return False
+
+
+    if not isinstance(first_image_encoding, list) or not isinstance(second_image_encoding, list):
+        raise ValueError("Both encodings must be of type 'list'.")
+
+    first_image_encoding = np.array(first_image_encoding)
+    second_image_encoding = np.array(second_image_encoding)
+
+
+
+    face_distance = face_recognition.face_distance([first_image_encoding], second_image_encoding)[0]
+    
+    # Determine if they are the same person based on the threshold
+    is_same_person = face_distance <= threshold
+
+    print(f"Same person? {is_same_person} with error_margin {face_distance}")
+    
+    return is_same_person
+
+
+def encode_image(image):
+    byte_image = face_recognition.load_image_file(BytesIO(image))
+    encoding = face_recognition.face_encodings(byte_image)
+
+    if encoding:
+        return encoding[0].tolist()
+
+    else:
+        return None
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_mysqldb import MySQL
 from os import getenv
 import detector
-
+import json
 
 app = Flask(__name__)
 
@@ -22,26 +22,70 @@ app.config["MEDIA_FOLDER"] = MEDIA_FOLDER
 
 def get_images_and_ids_from_db() -> tuple:
     cur = mysql.connection.cursor()
-    cur.execute("SELECT image, id FROM finder_app_contact where image is not null")
+    cur.execute("SELECT id, encoding FROM image_encodings where encoding is not null")
     data = cur.fetchall()
     cur.close()
     return data
 
+
 @app.route("/", methods=["POST"])
 def index():
     source_image = request.files["image"].read()
+    source_image_encoding = detector.encode_image(source_image)
+    
     data = get_images_and_ids_from_db()
 
     found_ids = []
 
-    for image, id in data:
-        if not image:
+    for id, encoding in data:
+        if not encoding:
             continue
-        second_image_path = f"{MEDIA_FOLDER}/{image}"
-        with open(second_image_path, "rb") as f:
-            second_image = f.read()
-            if detector.is_same_person(source_image, second_image):
-                found_ids.append(id)
-
+        if detector.is_same_person_optimized(source_image_encoding, json.loads(encoding)):
+            found_ids.append(id)
 
     return str(found_ids)
+
+
+@app.route("/images/encode", methods=["POST"])
+def encode():
+    source_image = request.files["image"].read()
+
+    encoded_image = detector.encode_image(source_image)
+
+    return encoded_image
+
+@app.route("/contacts/save", methods=["POST"])
+def save():
+    contact_id = request.json["id"]
+    encoded_image = request.json["encoded_image"]
+
+    if encoded_image is None:
+        return "No face encoding provided.", 400
+
+    cur = mysql.connection.cursor()
+
+    cur.execute("SELECT id FROM image_encodings WHERE id = %s", (contact_id,))
+    record = cur.fetchone()
+
+    if record:
+        cur.execute(
+            "UPDATE image_encodings SET encoding = %s WHERE id = %s",
+            (json.dumps(encoded_image), contact_id),
+        )
+        mysql.connection.commit()
+        response = f"Updated encoding for contact ID {contact_id}"
+    else:
+        cur.execute(
+            "INSERT INTO image_encodings (id, encoding) VALUES (%s, %s)",
+            (contact_id, json.dumps(encoded_image)),
+        )
+        mysql.connection.commit()
+        response = f"Inserted new encoding for contact ID {contact_id}"
+
+    cur.close()
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
